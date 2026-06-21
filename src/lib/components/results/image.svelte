78be04aa0ab84380be3dd21ca5d7cfb269806caf
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2026-06-21 04:40:12 +0000

    Better Image without thumnail handling.

--- src/lib/components/results/image.svelte
+++ src/lib/components/results/image.svelte
@@ -13,10 +13,16 @@
         {#each imageResults.results as result}
             <div class="border border-base-300 rounded-sm overflow-hidden min-w-30 max-w-40 sm:min-w-50 sm:max-w-60 shrink-0">
                 <div class="block w-full h-full">
-                    <Lightbox title={`${result.title} (${result.resolution})`}>
-                        <img slot="thumbnail" loading="lazy" class="object-cover w-full max-h-40" src={result.thumbnail_src} alt="Thumbnail">
-                        <img loading="lazy" src={result.img_src} alt={result.title} />
-                    </Lightbox>
+                    {#if (result.thumbnail_src)}
+                        <Lightbox title={`${result.title} (${result.resolution})`}>
+                            <img slot="thumbnail" loading="lazy" class="object-cover w-full max-h-40" src={result.thumbnail_src} alt="Thumbnail">
+                            <img loading="lazy" src={result.img_src} alt={result.title} />
+                        </Lightbox>
+                    {:else}
+                        <Lightbox title={`${result.title} (${result.resolution})`}>
+                            <img loading="lazy" src={result.img_src} alt={result.title} />
+                        </Lightbox>
+                    {/if}
                     <a href={result.url}  rel="noopener noreferrer" class="p-2">
                         <p class="text-sm font-medium line-clamp-2 hover:underline" title={result.title}>{result.title}</p>
                         <p class="text-xs opacity-60" title={result.url}>{new URL(result.url).hostname}</p>
